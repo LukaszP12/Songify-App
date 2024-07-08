a14cf54174098ddb9b5c1f234ee5ad42efac2be8
Lukasz Piwowarski <lukaszp4@onet.eu>
2024-07-08 23:05:00 +0200

    initial commit of songify

---
+++ src/main/java/songify/SongifyApplication.java
@@ -0,0 +1,11 @@
+package songify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongifyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SongifyApplication.class, args);
+    }
+}
