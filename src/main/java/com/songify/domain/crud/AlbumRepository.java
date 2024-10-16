619c0c7d2785c0467964b24e5d828010c0b077ae
Lukasz Piwowarski <lukaszp4@onet.eu>
2024-10-16 11:51:51 +0200

    added repository classes related to crud entities

---
+++ src/main/java/com/songify/domain/crud/AlbumRepository.java
@@ -0,0 +1,34 @@
+package com.songify.domain.crud;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+import java.util.Set;
+
+public interface AlbumRepository extends Repository<Album, Long> {
+
+    Album save(Album album);
+
+    @Query("""
+            select a from Album a 
+            join fetch a.songs songs
+            join fetch a.artists artists 
+            join fetch songs.genre g
+            where artists.id = :id
+            """)
+    Set<Album> findAllAlbumsByArtistId(@Param("id") Long id);
+
+    @Query("select a from Album a where a.id = :id")
+    Optional<AlbumInfo> findById(@Param("id") Long id);
+
+    @Query("""
+            select a from Album a
+            join fetch a.songs songs on a.id
+            join fetch a.artists artists
+            where a.id = :id 
+            """)
+    Optional<AlbumInfo> findAlbumByIdWithSongsAndArtists(Long id);
+
+}
---
+++ src/main/java/com/songify/domain/crud/ArtistRepository.java
@@ -0,0 +1,15 @@
+package com.songify.domain.crud;
+
+import com.songify.domain.crud.song.dto.ArtistDto;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.Repository;
+
+import java.util.Set;
+
+interface ArtistRepository extends Repository<Artist, Long> {
+    ArtistDto save(Artist artist);
+
+    Set<Artist> findAll();
+
+    Set<Artist> findAll(Pageable pageable);
+}
