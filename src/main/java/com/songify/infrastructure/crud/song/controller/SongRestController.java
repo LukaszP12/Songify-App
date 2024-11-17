cf7f376db53564c9bc80036e7bc092476a06bd03
Lukasz Piwowarski <lukaszp4@onet.eu>
2024-11-18 00:48:57 +0100

    added missing PUT and POST methods

--- src/main/java/com/songify/infrastructure/crud/song/controller/SongRestController.java
+++ src/main/java/com/songify/infrastructure/crud/song/controller/SongRestController.java
@@ -4,6 +4,7 @@ import com.songify.domain.crud.SongifyCrudFacade;
 import com.songify.domain.crud.song.dto.SongDto;
 import com.songify.infrastructure.crud.song.controller.dto.request.PartiallyUpdateSongRequestDto;
 import com.songify.infrastructure.crud.song.controller.dto.request.UpdateSongRequestDto;
+import com.songify.infrastructure.crud.song.controller.dto.response.CreateSongResponseDto;
 import com.songify.infrastructure.crud.song.controller.dto.response.DeleteSongResponseDto;
 import com.songify.infrastructure.crud.song.controller.dto.response.GetAllSongsResponseDto;
 import com.songify.infrastructure.crud.song.controller.dto.response.GetSongResponseDto;
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -52,26 +54,24 @@ public class SongRestController {
         return ResponseEntity.ok(response);
     }
 
-//    @GetMapping("/artist")
-//    public ResponseEntity<GetAllSongsResponseDto> getSongByArtist(@RequestHeader(required = false) String artist) {
-//        log.info(songFacade.findByArtistEquals(artist));
-//        List<Song> songs = songFacade.findByArtistEquals(artist);
-//        GetAllSongsResponseDto allSongs = SongControllerMapper.mapFromSongToGetAllSongsResponseDto(songs);
-//        return ResponseEntity.ok(allSongs);
-//    }
-
-//    @PostMapping
-//    ResponseEntity<CreateSongResponseDto> postSong(@RequestBody @Valid SongRequestDto request) {
-//        SongDto savedSong = songFacade.addSong(request);
-//        CreateSongResponseDto body = mapFromSongToCreateSongResponseDto(savedSong);
-//        return ResponseEntity.ok(body);
-//    }
+    @PostMapping
+    ResponseEntity<CreateSongResponseDto> postSong(@RequestBody @Valid SongRequestDto request) {
+        SongDto savedSong = songFacade.addSong(request);
+        CreateSongResponseDto body = mapFromSongToCreateSongResponseDto(savedSong);
+        return ResponseEntity.ok(body);
+    }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<DeleteSongResponseDto> deleteSongByIdUsingPathVariable(@PathVariable Long id) {
         songFacade.deleteById(id);
         DeleteSongResponseDto body = SongControllerMapper.mapFromSongToDeleteSongResponseDto(id);
         return ResponseEntity.ok(body);
+    }
+
+    @PutMapping("/{songId}/genres/{genreId}")
+    ResponseEntity<String> assignGenreToSong(@PathVariable Long songId,@PathVariable Long genreId){
+        songFacade.assignGenreToSong(genreId,songId);
+        return ResponseEntity.ok("updated");
     }
 
     @PutMapping("/{id}")
