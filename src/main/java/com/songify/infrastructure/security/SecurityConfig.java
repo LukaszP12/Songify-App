d796dca3495cc69fadbee939431ccc7eaa9d54a5
Lukasz Piwowarski <lukaszp4@onet.eu>
2024-12-08 23:27:45 +0100

    SecurityConfig with UserDetailsManager

--- src/main/java/com/songify/infrastructure/security/SecurityConfig.java
+++ src/main/java/com/songify/infrastructure/security/SecurityConfig.java
@@ -3,14 +3,14 @@ package com.songify.infrastructure.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 
 @Configuration
 class SecurityConfig {
 
     @Bean
-    public UserDetailsService userDetailsService() {
+    public UserDetailsManager userDetailsService() {
         var manager = new InMemoryUserDetailsManager();
         var user1 = User.withUsername("albert")
                 .password("12345")
