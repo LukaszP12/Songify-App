426039aadfb7bf67eff00b5a89b5de4852c2894a
Lukasz Piwowarski <lukaszp4@onet.eu>
2024-12-11 22:39:25 +0100

    additional method which should be adjusted

--- src/main/java/com/songify/infrastructure/security/SecurityConfig.java
+++ src/main/java/com/songify/infrastructure/security/SecurityConfig.java
@@ -88,7 +88,7 @@ class SecurityConfig {
                 config.setAllowedOrigins(
                         List.of("http://localhost:3000"));
                 config.setAllowedMethods(
-                        List.of("GET", "POST", "PUT", "DELETE"));
+                        List.of("GET", "POST", "PUT", "DELETE","PATCH"));
                 config.setAllowedHeaders(List.of("*"));
                 return config;
             };
