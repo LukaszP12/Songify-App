a177c414780dd6b8a6bcf0973380596aee13f6cc
Lukasz Piwowarski <lukaszp4@onet.eu>
2024-12-17 14:59:44 +0100

    JwtAuthTokenFilter which implements OncePerRequestFilter, so it is only invoked once. Request and Response are chained further.

---
+++ src/main/java/com/songify/infrastructure/security/jwt/JwtAuthTokenFilter.java
@@ -0,0 +1,30 @@
+package com.songify.infrastructure.security.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@Order(12)
+@Log4j2
+public class JwtAuthTokenFilter extends OncePerRequestFilter {
+
+    public JwtAuthTokenFilter() {
+        super();
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        log.info("request.getHeaderNames(): " + request.getHeaderNames());
+        filterChain.doFilter(request, response);
+    }
+}
