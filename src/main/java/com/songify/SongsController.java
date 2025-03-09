8c49cb20f6454b4886aa392f1faac31590460717
Lukasz Piwowarski <lukaszp4@onet.eu>
2025-03-09 22:41:47 +0100

    added requestHeader to getById method

--- src/main/java/com/songify/SongsController.java
+++ src/main/java/com/songify/SongsController.java
@@ -1,9 +1,11 @@
 package com.songify;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +14,7 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 @RestController
+@Log4j2
 public class SongsController {
 
     public static Map<Integer, String> database;
@@ -41,7 +44,9 @@ public class SongsController {
     }
 
     @GetMapping("/songs/{id}")
-    public ResponseEntity<SingleSongResponseDto> getSongById(@PathVariable Integer id) {
+    public ResponseEntity<SingleSongResponseDto> getSongById(@PathVariable Integer id,
+                                                             @RequestHeader(required = false) String requestId) {
+        log.info(requestId);
         String song = database.get(id);
         if (song == null) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
