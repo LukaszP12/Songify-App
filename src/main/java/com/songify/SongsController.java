71f8693afcd2778aead8934e254dbc84d12745bc
Lukasz Piwowarski <lukaszp4@onet.eu>
2025-03-09 11:01:23 +0100

    06.05.get data by id using PathVariable + RestController implementation

---
+++ src/main/java/com/songify/SongsController.java
@@ -0,0 +1,30 @@
+package com.songify;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class SongsController {
+
+    Map<Integer, String> database = new HashMap<>();
+
+    @GetMapping("/songs")
+    public ResponseEntity<SongResponseDto> getAllSongs() {
+        database.put(1, "shawnmendes song1");
+        database.put(2, "ariana grande song2");
+        SongResponseDto response = new SongResponseDto(database);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/songs/{id}")
+    public ResponseEntity<SingleSongResponseDto> getSongById(@PathVariable Integer id) {
+        String song = database.get(id);
+        SingleSongResponseDto response = new SingleSongResponseDto(song);
+        return ResponseEntity.ok(response);
+    }
+}
