cf0a60744687520835102be3b39d9d43416a708f
Lukasz Piwowarski <lukaszp4@onet.eu>
2025-03-10 00:36:11 +0100

    added validation for incoming request

--- src/main/java/com/songify/SongRequestDto.java
+++ src/main/java/com/songify/SongRequestDto.java
@@ -1,9 +1,11 @@
 package com.songify;
 
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 
 public record SongRequestDto(
         @NotNull(message = "songName must not be null")
+        @NotEmpty(message = "songName must not be empty")
         String songName
 ) {
 }
