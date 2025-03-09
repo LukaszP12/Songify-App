5d019761292b48b52d4f2f779f81c3f8ea343e59
Lukasz Piwowarski <lukaszp4@onet.eu>
2025-03-09 22:25:58 +0100

    06.05.get data by id using PathVariable + RestController implementation + if database record not present for given id, returns notFound status ammended + limitedMap

--- src/main/java/com/songify/SongsController.java
+++ src/main/java/com/songify/SongsController.java
@@ -4,20 +4,38 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 public class SongsController {
 
-    Map<Integer, String> database = new HashMap<>();
+    public static Map<Integer, String> database;
 
-    @GetMapping("/songs")
-    public ResponseEntity<SongResponseDto> getAllSongs() {
+    static {
+        database = new HashMap<>();
         database.put(1, "shawnmendes song1");
         database.put(2, "ariana grande song2");
+    }
+
+    @GetMapping("/songs")
+    public ResponseEntity<SongResponseDto> getAllSongs(@RequestParam(required = false) Integer limit) {
+        database.put(1, "shawnmendes song1");
+        database.put(2, "ariana grande song2");
+        database.put(3, "ariana grande song212123123");
+        database.put(4, "ariana grande song212123123cvncbcv");
+        if (limit != null) {
+            Map<Integer, String> limitedMap = database.entrySet()
+                    .stream()
+                    .limit(limit)
+                    .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+            SongResponseDto response = new SongResponseDto(limitedMap);
+            return ResponseEntity.ok(response);
+        }
         SongResponseDto response = new SongResponseDto(database);
         return ResponseEntity.ok(response);
     }
