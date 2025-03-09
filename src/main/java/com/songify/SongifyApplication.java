8bafe909fcbe7713164fb6b9719b1a8022a30502
Lukasz Piwowarski <lukaszp4@onet.eu>
2025-03-09 23:39:38 +0100

    added thymeleaf and corresponding controller

--- src/main/java/com/songify/SongifyApplication.java
+++ src/main/java/com/songify/SongifyApplication.java
@@ -3,8 +3,6 @@ package com.songify;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 public class SongifyApplication extends SpringBootServletInitializer {
