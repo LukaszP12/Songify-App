4dfdab4710243eaefdd075da74ff2efd8689e202
Lukasz Piwowarski <lukaszp4@onet.eu>
2025-03-12 22:55:42 +0100

    08.12 - amended SongAdder

--- src/main/java/com/songify/song/domain/service/SongAdder.java
+++ src/main/java/com/songify/song/domain/service/SongAdder.java
@@ -16,7 +16,7 @@ public class SongAdder {
     public Song addSong(Song song) {
         log.info("adding new song: " + song);
         // zapytanie do serwisu songs.com/validate?songName=song
-        songRepository.save(song);
-        return song;
+        Song savedSong = songRepository.save(song);
+        return savedSong;
     }
 }
