eb3bd8b6549097aa503521d615df79f079b6619b
Lukasz Piwowarski <lukaszp4@onet.eu>
2025-03-14 11:09:05 +0100

    08.23. - added @Transactional to classes which require them

--- src/main/java/com/songify/song/domain/service/SongAdder.java
+++ src/main/java/com/songify/song/domain/service/SongAdder.java
@@ -5,10 +5,12 @@ import com.songify.song.domain.repository.SongRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Log4j2
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class SongAdder {
 
     private final SongRepository songRepository;
--- src/main/java/com/songify/song/domain/service/SongDeleter.java
+++ src/main/java/com/songify/song/domain/service/SongDeleter.java
@@ -4,10 +4,12 @@ import com.songify.song.domain.repository.SongRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Log4j2
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class SongDeleter {
 
     private final SongRepository songRepository;
