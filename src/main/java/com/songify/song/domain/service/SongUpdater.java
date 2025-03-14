5e8a2e6acad9541caf4aaa89ae77afd0ba96164b
Lukasz Piwowarski <lukaszp4@onet.eu>
2025-03-14 08:59:18 +0100

    08.18. - dirty checking implementation, this should be used only for simple queris, for more complicated ones we should implement repositories.

--- src/main/java/com/songify/song/domain/service/SongUpdater.java
+++ src/main/java/com/songify/song/domain/service/SongUpdater.java
@@ -1,7 +1,6 @@
 package com.songify.song.domain.service;
 
 import com.songify.song.domain.entities.Song;
-import com.songify.song.domain.repository.SongRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
@@ -13,36 +12,27 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class SongUpdater {
 
-    private final SongRepository songRepository;
     private final SongRetriever songRetriever;
 
     public void updateById(Long id, Song newSong) {
-        songRetriever.existsById(id);
-        songRepository.updateById(id, newSong);
+        Song songById = songRetriever.findSongById(id);
+        songById.setName(newSong.getName());
+        songById.setArtist(newSong.getArtist());
     }
 
-    public Song updatePartiallyById(Long id, Song updatedSong) {
-        songRetriever.existsById(id);
+    public Song updatePartiallyById(Long id, Song songFromRequest) {
         Song songFromDatabase = songRetriever.findSongById(id);
-        Song.SongBuilder builder = Song.builder();
-        if (updatedSong.getName() != null) {
-            builder.name(updatedSong.getName());
+        if (songFromRequest.getName() != null) {
+            songFromDatabase.setName(songFromRequest.getName());
             log.info("partially updated song name");
-        } else {
-            builder.name(songFromDatabase.getName());
         }
-        if (updatedSong.getArtist() != null) {
-            builder.artist(updatedSong.getArtist());
+        if (songFromRequest.getArtist() != null) {
+            songFromDatabase.setArtist(songFromRequest.getArtist());
             log.info("partially updated artist name");
-        } else {
-            builder.artist(songFromDatabase.getArtist());
         }
-        ;
         log.info("Partially updated song with id: " + id +
-                " with oldSongName: " + songFromDatabase.getName() + " to newSongName: " + updatedSong.getName() +
-                " oldArtist: " + songFromDatabase.getArtist() + " to newArtist: " + updatedSong.getArtist());
-        Song toSave = builder.build();
-        updateById(id,toSave);
-        return toSave;
+                " with oldSongName: " + songFromDatabase.getName() + " to newSongName: " + songFromRequest.getName() +
+                " oldArtist: " + songFromDatabase.getArtist() + " to newArtist: " + songFromRequest.getArtist());
+        return songFromDatabase;
     }
 }
