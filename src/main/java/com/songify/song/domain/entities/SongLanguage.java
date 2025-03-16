fe2dd2429c04888db216124a897c98e2876ef42f
Lukasz Piwowarski <lukaszp4@onet.eu>
2025-03-16 12:00:14 +0100

    12.07 - enum class to store language of song

---
+++ src/main/java/com/songify/song/domain/entities/SongLanguage.java
@@ -0,0 +1,12 @@
+package com.songify.song.domain.entities;
+
+public enum SongLanguage {
+    ENGLISH,
+    SPANISH,
+    POLISH,
+    FRENCH,
+    GERMAN,
+    ITALIAN,
+    JAPANESE,
+    OTHER
+}
