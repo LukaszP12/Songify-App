560bbb90a90ae8cad480512a0f453057bdfc896f
Lukasz Piwowarski <lukaszp4@onet.eu>
2025-03-18 22:51:14 +0100

    12.34. - album with song features

---
+++ src/main/java/com/songify/domain/crud/AlbumAdder.java
@@ -0,0 +1,33 @@
+package com.songify.domain.crud;
+
+import com.songify.domain.crud.dto.AlbumDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Instant;
+import java.util.Set;
+
+@Log4j2
+@Service
+@RequiredArgsConstructor
+@Transactional
+class AlbumAdder {
+
+    private final SongRetriever songRetriever;
+    private final AlbumRepository albumRepository;
+
+    AlbumDto addAlbum(final Long songId, final String albumName, final Instant releaseDate) {
+        Song songById = songRetriever.findSongById(songId);
+
+        Album album = new Album();
+        album.setTitle(albumName);
+        album.setReleaseDate(releaseDate);
+        album.setSongs(Set.of(songById));
+
+        albumRepository.save(album);
+
+        return new AlbumDto(songById.getId(), albumName);
+    }
+}
